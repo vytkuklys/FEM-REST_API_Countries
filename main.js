41751759dbf3faa9e3979f5cf0b3afa041dfc3db
Vytautas <70965403+vytkuklys@users.noreply.github.com>
2021-05-09 23:52:08 +0300

    style detail page and load data from api to it

--- main.js
+++ main.js
@@ -1,31 +1,72 @@
-const regionToggle = document.querySelector('.controls__title');
-const modeSwitcher = document.querySelector('.header__button');
-const regions = document.querySelectorAll('.controls__select-item');
+const regionToggle  = document.querySelector('.controls__title');
+const modeSwitcher  = document.querySelector('.header__button');
+const searchInput   = document.querySelector('.controls__input-text');
+const regions       = document.querySelectorAll('.controls__select-item');
 
 async function getAllCountries() {
-    const response = await fetch('https://restcountries.eu/rest/v2/all');
+    const response = await fetch('https://restcountries.eu/rest/v2/all?fields=flag;name;capital;population;region');
     const json = await response.json();
     json.forEach((country, i) => {
         displayElements(country.flag, country.name, country.capital, country.region, country.population);
     });
+    addEvents();
 }
 
 async function getSearchedCountry(country) {
-    const response = await fetch(`https://restcountries.eu/rest/v2/name/${country}`);
-    const json = await response.json();
-    console.log(json[0].capital);
-    json.forEach((country, i) => {
-        displayElements(country.flag, country.name, country.capital, country.region, country.population);
-    });
+    try{
+        // const response = await fetch(`https://restcountries.eu/rest/v2/name/${country}?fields=flag;name;capital;population;region;nativeName;subregion;currencies;languages;borders;topLevelDomain`);
+        const response = await fetch(`https://restcountries.eu/rest/v2/name/${country}?fields=flag;name;capital;population;region`);
+        if(response.status !== 404){
+            const json = await response.json();
+            clearAllCountries();
+            console.log(json[0].name)
+            json.forEach((country, i) => {
+                displayElements(country.flag, country.name, country.capital, country.region, country.population);
+            });
+        }
+            // if(country === 'belgium' || country === 'latvia'){
+            //     const img = document.querySelector('.details__flag');
+            //     img.setAttribute('src', `${json[0].flag}`);
+            //     const lists = document.querySelectorAll('.details__list-items');
+            //     lists.forEach(list =>{
+            //         const items = Array.from(list.children);
+            //         items.forEach((item) => {
+            //             const id = item.id;
+            //             if(typeof(json[0][id]) !== 'object'){
+            //                 item.children[0].textContent = json[0][id];
+            //             }else if(id === 'topLevelDomain'){
+            //                item.children[0].textContent = json[0].topLevelDomain.join(', ')
+            //             }else if(id === 'currencies' || id === 'languages'){
+            //                 item.children[0].textContent = Array.from(Object.values(json[0][id]), cur => cur.name).join(', ');
+            //             }else{
+            //             }
+            //         });
+            //     })
+            //     console.log(Array.from(Object.values(json[0].borders), border => border.name));
+            //     console.log(json[0].borders);
+                
+            // }
+            //     // json.forEach((country, i) => {
+            //         //     displayElements(country.flag, country.name, country.capital, country.region, country.population);
+            //         // });
+            //     }
+            //     else{
+            //         clearAllCountries();
+                    
+            //     }
+            }catch(e){
+                console.log(e);
+            }
 }
 
 async function getRegion(region) {
-    const response = await fetch(`https://restcountries.eu/rest/v2/region/${region}`);
+    const response = await fetch(`https://restcountries.eu/rest/v2/region/${region}?fields=flag;name;capital;population;region`);
     const json = await response.json();
     clearAllCountries();
     json.forEach((country, i) => {
         displayElements(country.flag, country.name, country.capital, country.region, country.population);
     });
+    addEvents();
 }
 
 function displayElements(flag, country, capital, region, population) {
@@ -86,6 +127,16 @@ function displayElements(flag, country, capital, region, population) {
 
 }
 
+const getCountryData = async country =>{
+    try{
+        const response = await fetch(`https://restcountries.eu/rest/v2/name/${country}?fields=flag;name;capital;population;region;nativeName;subregion;currencies;languages;borders;topLevelDomain`);
+        const json = await response.json();
+        return json;
+    }catch(e){
+        console.log(e);
+    }
+}
+
 const clearAllCountries = () => {
     const output = document.getElementById('output');
     while (output.firstChild) {
@@ -121,11 +172,66 @@ const handleClickSearchRegion = (region)=>{
     getRegion(region.toLowerCase());
 }
 
-regionToggle.addEventListener('click', (e) => handleClickShowRegions(e));
+const showCountryDetails = async country =>{
+    const json = await getCountryData(country);
+    if(!json) return;
+    console.log(json);
+    const img   = document.querySelector('.details__flag');
+    const title = document.querySelector('.details__title');
+    const lists = document.querySelectorAll('.details__list-items');
+
+    img.setAttribute('src', `${json[0].flag}`);
+    title.textContent = json[0].name;
+    lists.forEach(list =>{
+        const items = Array.from(list.children);
+        items.forEach((item) => {
+            const id = item.id;
+            if(typeof(json[0][id]) === 'string'){
+                item.children[0].textContent = json[0][id];
+            }else if(id === 'population'){
+                item.children[0].textContent = json[0][id].toLocaleString('en');
+            }
+            else if(id === 'currencies' || id === 'languages'){
+                item.children[0].textContent = Array.from(Object.values(json[0][id]), cur => cur.name).join(', ');
+            }else if(id === 'topLevelDomain'){
+                item.children[0].textContent = json[0].topLevelDomain.join(', ')
+            }
+            else{
+            }
+        });
+    })
+    // console.log(json[0].borders);
+
+}
+
+const addEvents = () =>{
+    const cards = document.querySelectorAll('.output__box');
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            const country = card.children[1].children[0].textContent;
+            showCountryDetails(country);
+        });
+    })
+}
+// if(false){
+    regionToggle.addEventListener('click', (e) => handleClickShowRegions(e));
+    
+    
+    regions.forEach(region =>{
+        region.addEventListener('click', ()=> handleClickSearchRegion(region.textContent));
+    });
+    
+    searchInput.addEventListener("keyup", event => {
+        if (event.isComposing || event.keyCode === 229) {
+          return;
+        }
+        if(searchInput.value){
+            getSearchedCountry(searchInput.value);
+        }
+      });
+    getAllCountries();
+
+    
+// }
 
 modeSwitcher.addEventListener('click', (e) => handleClickSwitchMode(e));
-
-regions.forEach(region =>{
-    region.addEventListener('click', ()=> handleClickSearchRegion(region.textContent));
-});
-getAllCountries();
