5acadd77f72fca77a5d88485f6eff0c8f6ad778d
Vytautas <70965403+vytkuklys@users.noreply.github.com>
2021-11-23 23:39:23 +0100

    update api links

--- js/apiCalls.js
+++ js/apiCalls.js
@@ -2,7 +2,7 @@ import LocalStorage from './Localstorage.js';
 
 export const getAllCountries = async () => {
     try {
-        const response = await fetch('https://restcountries.eu/rest/v2/all?fields=flag;name;capital;population;region');
+        const response = await fetch('https://restcountries.com/v2/all?fields=flag,name,capital,population,region');
         const json = await response.json();
         return json;
     } catch (e) {
@@ -13,7 +13,7 @@ export const getAllCountries = async () => {
 
 export const getSelectedRegion = async region => {
     try {
-        const response = await fetch(`https://restcountries.eu/rest/v2/region/${region}?fields=flag;name;capital;population;region`);
+        const response = await fetch(`https://restcountries.com/v2/region/${region}?fields=flag,name,capital,population,region`);
         const json = await response.json();
         return json;
     } catch (e) {
@@ -24,7 +24,7 @@ export const getSelectedRegion = async region => {
 
 export const getSearchedCountry = async country => {
     try {
-        const response = await fetch(`https://restcountries.eu/rest/v2/name/${country}?fields=flag;name;capital;population;region`);
+        const response = await fetch(`https://restcountries.com/v2/name/${country}?fields=flag,name,capital,population,region`);
         if (response.status !== 404) {
             const json = await response.json();
             return json;
@@ -37,7 +37,7 @@ export const getSearchedCountry = async country => {
 
 export const getCountryData = async country => {
     try {
-        const response = await fetch(`https://restcountries.eu/rest/v2/name/${country}?fields=flag;name;capital;population;region;nativeName;subregion;currencies;languages;borders;topLevelDomain`);
+        const response = await fetch(`https://restcountries.com/v2/name/${country}?fields=flag,name,capital,population,region,nativeName,subregion,currencies,languages,borders,topLevelDomain`);
         const json = await response.json();
         return json;
     } catch (e) {
@@ -48,7 +48,7 @@ export const getCountryData = async country => {
 
 export const getBorderNames = async borders => {
     try {
-        const response = await fetch(`https://restcountries.eu/rest/v2/all?fields=name;alpha3Code`);
+        const response = await fetch(`https://restcountries.com/v2/all?fields=name,alpha3Code`);
         const json = await response.json();
         LocalStorage.addCountries(json);
         return LocalStorage.getRelevantCountries(borders);
